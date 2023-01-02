1de63abed2c12cc79860027eac894ae52060800e
s27081 <115358253+s27081@users.noreply.github.com>
2023-01-02 20:16:56 +0100

    Add files via upload

---
+++ zad 6.4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main()
+{
+	string word;
+	string addWord;
+	int n;
+
+	cout << "Podaj wyraz do konkatenacji: ";
+	cin >> word;
+	addWord = word;
+
+	cout << "Podaj ilosc konkatenacji: ";
+	cin >> n;
+
+
+	for (int i = 1; i < n; i++) {
+		word += addWord;
+	}
+
+	cout << word;
+}
+
