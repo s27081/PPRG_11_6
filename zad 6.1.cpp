088e03247cca08211a488af06f89e38650b7ecdc
s27081 <115358253+s27081@users.noreply.github.com>
2023-01-02 19:52:39 +0100

    Add files via upload

---
+++ zad 6.1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int nValue = 0;
+	int counter = 0;
+
+	cout << "Podaj liczbe n: ";
+	cin >> nValue;
+
+	for (int i = 1; i <= nValue; i++) {
+		if (i % 3 != 0) {
+			if (i % 5 == 0) {
+				counter++;
+			}
+		}
+	}
+
+	cout << "Sa " << counter << " liczby naturalne spelniajace wymagania";
+}
+
