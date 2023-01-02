ed65487542b5c18da4eef4596bb4afa8235df31d
s27081 <115358253+s27081@users.noreply.github.com>
2023-01-02 20:02:30 +0100

    Update zad 6.2.cpp

--- zad 6.2.cpp
+++ zad 6.2.cpp
@@ -1,39 +1,38 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cstdlib>
 
 using namespace std;
 
-void generateMarks(int arr[6][4]) {
-	for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 4; j++) {
-			arr[i][j] = (rand() % (6 - 1 + 1)) + 1;
-		}
-	}
-
-}
 
 int main()
 {
-	
+	string subjects[4] = { "informatyka","matematyka","biologia","jezyk polski" };
 	string studentData[6];
 	int studentMarks[6][4];
 	int studentNumb = 0;
 	int subjectNumb = 0;
+	int loop = 1;
 
 	for (int i = 0; i < 6; i++) {
 		cout << "Podaj imie ucznia nr " << i << ":";
 		cin >> studentData[i];
+		for (int j = 0; j < 4; j++) {
+			cout << "Podaj ocene z " << subjects[j] << ": ";
+			cin >> studentMarks[i][j];
+		}
 	}
 
-	generateMarks(studentMarks);
+	while (loop == 1) {
+		cout << "Podaj numer ucznia(0-5): ";
+		cin >> studentNumb;
 
-	cout << "Podaj numer ucznia(0-5): ";
-	cin >> studentNumb;
+		cout << "Podaj numer przedmiotu(0-informatyka,1-matematyka,2-biologia,3-jezyk polski): ";
+		cin >> subjectNumb;
 
-	cout << "Podaj numer przedmiotu(0-informatyka,1-matematyka,2-biologia,3-jezyk polski): ";
-	cin >> subjectNumb;
+		cout << "Ocena ucznia " << studentData[studentNumb] << " :" << studentMarks[studentNumb][subjectNumb] << endl;
 
-	cout << "Ocena ucznia " << studentData[studentNumb] << " :" << studentMarks[studentNumb][subjectNumb];
+	}
+	
 
 }
 
